26d150cb6c7a1af2816be7e68aff550594e45364
HERZI ATEB <mehdi.herzi.ateb@accenture.com>
2023-05-17 18:10:08 +0200

    009 change Line 11 npm, install  file befor_install for deployé

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,11 +2,13 @@
 
 # Installer les dépendances système nécessaires
 apt-get update
-apt-get install -y curl
+apt-get install -y curl 
 
 # Installer Node.js et npm
-curl -sL https://deb.nodesource.com/setup_14.x | bash -
+curl -sL https://deb.nodesource.com/setup_18.x | bash -
 apt-get install -y nodejs
+
+apt install npm -y
 
 # Installer les dépendances de l'application
 npm install -s 
