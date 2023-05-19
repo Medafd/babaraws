8a5a82c97db624456b790345ae3aaea0656782e0
HERZI ATEB <mehdi.herzi.ateb@accenture.com>
2023-05-19 16:22:50 +0200

    015 change file beforinstall

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
-
-sudo apt autoremove -y
-
-# Installer les dépendances système nécessaires
-sudo apt-get update -y
-sudo apt-get install -y curl
-
-# Installer Node.js et npm
-curl -sL https://deb.nodesource.com/setup_18.x | bash -
-sudo apt-get install -y nodejs
-
-sudo apt-get update -y
-
-#crée le fichier directory 
-sudo apt install npm -y  
-
-# Installer les dépendances de l'application
-sudo  npm install -y
+# Install node.js and PM2 globally
+sudo apt-get update
+sudo apt-get install nodejs-legacy -y
+sudo apt-get install npm  -y
+sudo npm install pm2 -g
